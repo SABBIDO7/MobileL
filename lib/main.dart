c96c779f48710a04b5c53ac6c7ddd94146cc6787
SABBIDO007 <sabbidinewalid12345@gmail.com>
2023-05-10 22:14:42 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'package:mobile_project/utils/colors.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Twitch Clone Tutorial',
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+        appBarTheme: AppBarTheme.of(context).copyWith(
+          backgroundColor: backgroundColor,
+          elevation: 0,
+          titleTextStyle: const TextStyle(
+            color: primaryColor,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+          iconTheme: const IconThemeData(
+            color: primaryColor,
+          ),
+        ),
+      ),
+
+
+    );
+  }
+}
